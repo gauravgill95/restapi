637d9a89b1abe762e7ce1e0a9fa1d62f73797e5d
Gaurav <43656222+gauravgill95@users.noreply.github.com>
2018-12-02 06:04:28 +0530

    Add files via upload

--- src/myscript.js
+++ src/myscript.js
@@ -2,16 +2,45 @@
       var i=1;  
       var limit=10;
       $('#add').click(function(){  
-           i++;
-           if(i<limit)  
+        
+          // console.log(i);
+           if(i<limit){  
            $('#dynamic_field').append('<tr id="row'+i+'"><td><input type="text" name="name" placeholder="Enter your item" class="form-control name_list" /></td><td><select id="Category" name="Category"><option value="Animals">Animals</option><option value="Flowers">Flowers</option><option value="Fruits">Fruits</option></select></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');  
-      });  
-      $(document).on('click', '.btn_remove', function(){  
-           var button_id = $(this).attr("id");   
+               i++;
+        }
+      }); 
+      var j=1; 
+      $('#add_c').click(function(){
+          
+          console.log()
+          if(j<limit){
+            j++;
+             var k=10+j;
+           $('#dynamic_field_c').append('<tr id="row'+k+'"><td><input type="text" name="name" placeholder="Enter your item" class="form-control name_list" /></td><td><button type="button" name="remove" id="'+k+'" class="btn btn-danger btn_remove">X</button></td></tr>');  
+          }
+      });
+      $(document).on('click', '.btn_remove', function(){
+           var button_id = $(this).attr("id"); 
+           if(button_id>10) j--;
+           else i--;  
            $('#row'+button_id+'').remove();  
-      });  
+          //  console.log(i);
+      });
+
+      function myFunction() {
+          var form=$("#dynamic_field").closest('form')
+          $(form.prop('elements')).each(function(){
+              var missing= $(this).val()===""
+              $(this).parent().toggleClass('error',missing)
+          })
+          return 0;
+      }
       $("#submit").click(function(){
+        if(myFunction())
         $("#add_name").submit();
+      })
+      $("#submit_c").click(function(){
+        $("#add_name_c").submit();
       })
       /*
       $('#submit').click(function(){            
